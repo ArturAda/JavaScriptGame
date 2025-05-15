1009bf7cc39c6ca8c96cb5ff3bc8d07286f2578d
arturadamov <adamov.ad@phystech.edu>
2025-05-15 22:16:28 +0300

    fix leaderboard.js

--- src/scripts/leaderboard.js
+++ src/scripts/leaderboard.js
@@ -18,7 +18,10 @@ export async function submitScoreToServer(score) {
 }
 
 export async function loadLeaderboard() {
-    const res = await fetch('/api/leaderboard');
+    const res = await fetch('/api/leaderboard', {
+        headers: { 'X-API-Key': 'Dragon_Bobik' }
+    });
+    if (!res.ok) throw new Error(await res.text());
     const data = await res.json();
     data.sort((a, b) => {
         if (currentSort.key === 'name') {
